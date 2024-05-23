b9acd2e9fca13a28ae337070cc55edff83ce1cd6
joaojota17 <jvictor.jca@gmail.com>
2024-05-23 07:50:06 -0300

    Add files via upload

---
+++ CadastroLogin/src/cadastrologin/Cadastro12.java
@@ -0,0 +1,19 @@
+
+package cadastrologin;
+
+
+public class Cadastro12 {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void pack() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ CadastroLogin/src/cadastrologin/CadastroLogin.java
@@ -0,0 +1,20 @@
+
+package cadastrologin;
+
+
+public class CadastroLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+        
+        
+            }
+        }
+
